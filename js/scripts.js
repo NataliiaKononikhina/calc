0ef50ce4c2594d0a1ad06b08c2f4c9e6214a5237
Nataliia Kononikhina <kononikhinan@gmail.com>
2017-10-29 10:06:00 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var button = document.querySelectorAll("button");
+var input = document.querySelector(".culc__input");
+
+button.addEventListener("click", function (evt) {
+    document.getElementById('input').innerHTML = result;
+});
